c05ef669b233dd968322240f411e9e3c6039db63
aaaus <aaaus@DESKTOP-OLT459U>
2022-08-26 16:12:44 -0500

    Add project files.

---
+++ First Project 1730/Form1.cs
@@ -0,0 +1,17 @@
+﻿1730-Austin-Atz
+
+namespace First_Project_1730
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
